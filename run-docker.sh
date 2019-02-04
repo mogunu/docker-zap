0d2b97682161a1c0a0f74b27518cd5081644d10e
mogunu <mogunu@gmail.com>
2019-02-04 19:29:37 +0530

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-which docker
+printf "which docker"
 CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true)
 
 # the target URL for ZAP to scan
